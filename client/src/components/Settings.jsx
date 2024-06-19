338e27793e55026bab5896a9c790e62a1a941214
sarthak <thakresarthak6@gmail.com>
2024-06-19 21:08:31 +0530

    setting frontend created

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -1,7 +1,28 @@
+import "../styles/Settings.css";
+
 function settings() {
   return (
-    <h1>settings</h1>
-  )
+    <>
+      <h2>Settings</h2>
+      <div className="userDetails">
+        <div className="userInfoSettings">
+          <input type="text" placeholder="Name" />
+        </div>
+        <div className="userInfoSettings">
+          <input type="text" placeholder="Email" />
+        </div>
+        <div className="userInfoSettings">
+          <input type="password" placeholder="Old Password" />
+        </div>
+        <div className="userInfoSettings">
+          <input type="password" placeholder="New Password" />
+        </div>
+        <div className="updateBtn">
+          <button className="update">Update</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default settings
