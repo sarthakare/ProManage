cbe350080b30f679a3b92f5a7c3033756d03d093
sarthak <thakresarthak6@gmail.com>
2024-06-20 12:24:35 +0530

    Analytics fronend created

--- client/src/components/Analytics.jsx
+++ client/src/components/Analytics.jsx
@@ -1,9 +1,57 @@
+import "../styles/Analytics.css";
+
 function Analytics() {
   return (
     <div>
       <h1>Analytics</h1>
+      <div className="task-dashboard">
+        <div className="task-list">
+          <div className="task-item">
+            <span className="task-dot"></span>
+            <span className="task-name">Backlog Tasks</span>
+            <span className="task-count">16</span>
+          </div>
+          <div className="task-item">
+            <span className="task-dot"></span>
+            <span className="task-name">To-do Tasks</span>
+            <span className="task-count">14</span>
+          </div>
+          <div className="task-item">
+            <span className="task-dot"></span>
+            <span className="task-name">In-Progress Tasks</span>
+            <span className="task-count">03</span>
+          </div>
+          <div className="task-item">
+            <span className="task-dot"></span>
+            <span className="task-name">Completed Tasks</span>
+            <span className="task-count">22</span>
+          </div>
+        </div>
+        <div className="task-list">
+          <div className="task-item">
+            <span className="task-dot"></span>
+            <span className="task-name">Low Priority</span>
+            <span className="task-count">16</span>
+          </div>
+          <div className="task-item">
+            <span className="task-dot"></span>
+            <span className="task-name">Moderate Priority</span>
+            <span className="task-count">14</span>
+          </div>
+          <div className="task-item">
+            <span className="task-dot"></span>
+            <span className="task-name">High Priority</span>
+            <span className="task-count">03</span>
+          </div>
+          <div className="task-item">
+            <span className="task-dot"></span>
+            <span className="task-name">Due Date Tasks</span>
+            <span className="task-count">03</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Analytics
